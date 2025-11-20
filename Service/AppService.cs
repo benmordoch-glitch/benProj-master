3947f1c4a76be0461522b0164893488629df84d6
tihonist <tihonist@DESKTOP-SLDKRTY>
2025-11-20 11:26:31 +0200

    Continued Path Page 20/11

--- Service/AppService.cs
+++ Service/AppService.cs
@@ -13,6 +13,7 @@ namespace benProj.Service
 
         private User user;
         private List<Cours> cours;
+        //private List<Path> paths;
         
 
         public static AppService GetInstance()
@@ -36,10 +37,16 @@ namespace benProj.Service
         {
             cours = new List<Cours>()
             {
-                 new Cours { Id = "1", CourseName = ":ריצה בים", Difficulty = 3, Distance = 5 },
-                 new Cours { Id = "2", CourseName = ":ריצה בטיילת", Difficulty = 2, Distance = 4 },
-                 new Cours { Id = "3", CourseName = ":ריצה dsfsdf", Difficulty = 1, Distance = 4 }
+                 new Cours { Id = "1", CourseName = "ריצה בים", Difficulty = 3, Distance = 8 },
+                 new Cours { Id = "2", CourseName = "ריצה בטיילת", Difficulty = 2, Distance = 5 },
+                 new Cours { Id = "3", CourseName = "מרוץ אייל 25", Difficulty = 5, Distance = 15 }
             };
+            //cours = new List<Path>()
+            //{
+            //     new Path { Id = "1", PathName = "ריצה בים" ,Distance = 8 },
+            //     new Path { Id = "2", PathName = "ריצה בטיילת" ,Distance = 5 },
+            //     new Path { Id = "3", PathName = "מרוץ אייל 25", Distance = 15 }
+            //};
         }
         public List<Cours> GetCours()
         {
--- Views/Paths.xaml.cs
+++ Views/Paths.xaml.cs
@@ -1,3 +1,5 @@
+using benProj.ViewModels;
+
 namespace benProj.Views;
 
 public partial class Paths : ContentPage
@@ -5,5 +7,6 @@ public partial class Paths : ContentPage
 	public Paths()
 	{
 		InitializeComponent();
-	}
+        BindingContext = new PathViewModel();
+    }
 }
