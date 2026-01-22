d22acbd819793ea4c5f4a87b61446317162c86a2
aleqs83 <aleqsavoyan@mail.com>
2026-01-22 11:26:39 +0200

    Last Update for 22.01 added CommuntyToolKit

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace benProj
 {
@@ -9,6 +10,7 @@ namespace benProj
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
